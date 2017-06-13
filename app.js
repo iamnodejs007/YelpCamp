bc7c39483024967402855304dea07d3c24b62ed9
Khuram <knshaikh@gmail.com>
2017-06-13 00:10:44 -0600

    new camps post request

--- app.js
+++ app.js
@@ -2,11 +2,18 @@
 
 const express     = require('express');
 const open        = require('open');
+const bodyParser  = require('body-parser');
 const app         = express();
-var PORT          = process.env.PORT || 5000;
+const PORT          = process.env.PORT || 5000;
+var campgrounds   = [
+                      {name: "Banff campground", image: "images/banff.jpg"},
+                      {name: "Canmore campground", image: "images/lake.jpg"},
+                      {name: "Lake Louise campground", image: "images/canmore.jpg"}
+                    ];
 
 app.set("view engine", "ejs");
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
 
@@ -14,12 +21,20 @@ app.get("/", function(req, res) {
 });
 
 app.get("/campgrounds", function(req, res) {
-   var campgrounds = [
-    {name: "Banff campground", image: "images/banff.jpg"},
-    {name: "Canmore campground", image: "images/lake.jpg"},
-    {name: "Lake Louise campground", image: "images/canmore.jpg"}
-  ];
+
   res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res) {
+  var name  = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
+  res.redirect("campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res) {
+  res.render("new");
 });
 
 app.listen(PORT, function(err) {
