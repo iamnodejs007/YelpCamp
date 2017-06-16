aecd917039c20bb2ab86ba5ca3f59e94100c0b35
Khuram <knshaikh@gmail.com>
2017-06-16 15:26:45 -0600

    comment update route

--- routes/comments.js
+++ routes/comments.js
@@ -39,6 +39,7 @@ router.post("/", isLoggedIn, function(req, res) {
   });
 });
 
+//Edit comments GET Route
 router.get("/:comment_id/edit", function(req, res) {
   Comment.findById(req.params.comment_id, function(err, comment) {
     if (err) {
@@ -46,6 +47,18 @@ router.get("/:comment_id/edit", function(req, res) {
       res.redirect("back");
     } else {
   res.render("comments/edit", {campground_id: req.params.id, comment: comment});
+    }
+  });
+});
+
+//Update comments PUT Route
+router.put("/:comment_id", function(req, res) {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, comment) {
+    if (err) {
+      console.log(err);
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
     }
   });
 });
